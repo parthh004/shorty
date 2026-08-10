bcb1be99a0399f4d006ad728012db4869cac25de
Dev Chauhan <dev.chauhan@tssconsultancy.com>
2026-08-10 15:34:49 +0530

    user entity added

---
+++ src/main/java/com/tss/shorty/entity/User.java
@@ -0,0 +1,61 @@
+package com.tss.shorty.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.sql.Timestamp;
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(columnDefinition = "UUID", updatable = false, nullable = false)
+    private UUID userId;
+
+    @Column(nullable = false)
+    private String userName;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false, unique = true)
+    private String phoneNo;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private Role role;
+
+    @Column(nullable = false)
+    private Boolean isActive = true;
+
+    @Column(nullable = false)
+    private Boolean isEmailVerified = false;
+
+    @Column
+    private String profilePicture;
+
+    @Column(nullable = false)
+    private Integer availableSlots;
+
+    @Column(nullable = false, updatable = false)
+    @CreationTimestamp
+    private Timestamp createdOn;
+
+    @Column(nullable = false)
+    @UpdateTimestamp
+    private Timestamp updatedOn;
+}
