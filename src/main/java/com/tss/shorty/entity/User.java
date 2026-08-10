7a0a634ebd6705907a9d518276dc2af2d9903b40
DevChauhan2710 <chauhandev537@gmail.com>
2026-08-10 16:35:06 +0530

    file structure added

--- src/main/java/com/tss/shorty/entity/User.java
+++ src/main/java/com/tss/shorty/entity/User.java
@@ -7,7 +7,6 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
-
 import java.sql.Timestamp;
 import java.util.UUID;
 
@@ -36,7 +35,7 @@ public class User
     @Column(nullable = false, unique = true)
     private String email;
 
-    @Column(nullable = false)
+    @Column(nullable = false, updatable = false)
     private Role role;
 
     @Column(nullable = false)
