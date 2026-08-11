0582ffdf857f90bdcf218c06112aa658a7d2e9ef
parthh004 <parthsg004@gmail.com>
2026-08-11 18:55:56 +0530

    pagination patch

---
+++ src/main/java/com/tss/shorty/payload/response/PaginatedDto.java
@@ -0,0 +1,25 @@
+package com.tss.shorty.payload.response;
+
+import lombok.Data;
+import org.springframework.data.domain.Page;
+
+import java.util.List;
+
+@Data
+public class PaginatedDto<T> {
+    private List<T> content;
+    private int pageNumber;
+    private int pageSize;
+    private long totalElements;
+    private int totalPages;
+    private boolean last;
+
+    public PaginatedDto(Page<T> page) {
+        this.content = page.getContent();
+        this.pageNumber = page.getNumber();
+        this.pageSize = page.getSize();
+        this.totalElements = page.getTotalElements();
+        this.totalPages = page.getTotalPages();
+        this.last = page.isLast();
+    }
+}
